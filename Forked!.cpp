9766ff766b006107d4951d0283f5bf6d14e2f4ca
Sam@2004# <samarpjain01@gmail.com>
2024-04-11 18:49:35 +0530

    new code

---
+++ Forked!.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define pb push_back
+#define ppb pop_back
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define vpi vector<pair<int, int>>
+#define yes cout<<"YES"<<endl
+#define no cout<<"NO"<<endl
+
+int main()
+{
+    ll t;
+    cin >> t;
+    while (t--) {
+        ll n;
+        cin >> n;
+        // Your code here
+    }
+    return 0;
+}
